2ee229623bce326071a3caff65c7222dbb9ad3c5
Sothiry In <insothiry@gmail.com>
2025-10-17 18:57:20 +0700

    user filter and fix ui

--- app/admin/overview/layout.jsx
+++ app/admin/overview/layout.jsx
@@ -39,6 +39,8 @@ export default function Layout({ children }) {
 		queryFn: getMyDetails,
 	});
 
+	console.log("my de: ", user_data);
+
 	const handleLogout = () => {
 		logout();
 		router.push("/signin");
