4f3b8711997a1c7e07b808f374a231fd454a93d4
Sopanha-KE <sopanha.suon@khmerenterprise.info>
2025-10-20 05:59:58 +0700

    update leave page

--- app/admin/overview/leaves/page.jsx
+++ app/admin/overview/leaves/page.jsx
@@ -33,7 +33,7 @@ import "react-date-range/dist/theme/default.css";
 import AddLeaveDialog from "./components/addleavedialog";
 import UserProfileSection from "./components/user-profile-section";
 import { DateRangePicker } from "react-date-range";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getLeave } from "@/lib/api/adminLeave";
 
 const ALL = [
@@ -224,6 +224,8 @@ const Leaves = () => {
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [selectedEmployee, setSelectedEmployee] = useState(null);
 
+  const queryClient = useQueryClient();
+
   // Fetch leave data from API
   const {
     data: leaveResponse,
@@ -300,6 +302,14 @@ const Leaves = () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
+
+  useEffect(() => {
+    if (selectedRange.startDate && selectedRange.endDate) {
+      queryClient.invalidateQueries({
+        queryKey: ["leave", selectedRange.startDate, selectedRange.endDate],
+      });
+    }
+  }, [selectedRange, company?.id, queryClient]);
 
   if (isLoading) {
     return (
@@ -426,6 +436,10 @@ const Leaves = () => {
                       startDate: today,
                       endDate: today,
                       key: "selection",
+                    });
+
+                    queryClient.invalidateQueries({
+                      queryKey: ["leave", selectedRange.startDate, selectedRange.endDate],
                     });
                   }}
                   className="font-custom rounded-full border border-gray-400 flex items-center justify-between w-auto h-9 text-white"
