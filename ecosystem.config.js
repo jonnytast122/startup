3b7c2afd665b2791ae879f3270b4773ecd15a12e
vieysokheng <116995143+jamieheng@users.noreply.github.com>
2025-11-06 18:58:34 +0700

    change ecosystem to js

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "anan-client",
+      cwd: "/root/anan-client",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      interpreter: "node",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
